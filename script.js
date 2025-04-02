041ca0be56102147e4bcc657e5d4d229e7ca2c2a
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-02 18:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// let submit = document.getElementById("submit");
-let email = document.getElementById("email");
-let number = document.getElementById("number");
-let firstName = document.getElementById("name");
-let lastName = document.getElementById("name");
+cy.visit(baseUrl);
+cy.get('input[name="First Name"]').type('John');
+cy.get('input[name="Last Name"]').type('Doe');
+cy.get('input[name="Phone Number"]').type('1234567890');
+cy.get('input[name="Email ID"]').type('john.doe@example.com');
+cy.get('button[type="submit"]').click();
 
-function submit(){
-	alert("First Name: " + firstName.value +" " + "Last Name: " + lastName.value + " " + "Phone Number: " + number.value +" " + "Email ID: " + email.value )
-}
-document.getElementById("submit").addEventListener("click", submit);
+cy.on('window:alert', (alertText) => {
+	expect(alertText).to.contain('First Name: John');
+	expect(alertText).to.contain('Last Name: Doe');
+	expect(alertText).to.contain('Phone Number: 1234567890');
+	expect(alertText).to.contain('Email ID: john.doe@example.com');
+});
